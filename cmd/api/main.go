362dbc24978272664510ed977042232f6eeebd0c
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-11-19 19:07:42 -0300

    feat: carregando config e database no main. identity

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "log/slog"
+import (
+	"context"
+	"log/slog"
+
+	"github.com/marcelofabianov/cashly/config"
+	app "github.com/marcelofabianov/cashly/internal"
+	"github.com/marcelofabianov/cashly/pkg/database"
+)
 
 func main() {
 	run()
@@ -9,7 +16,47 @@ func main() {
 }
 
 func run() error {
-	//...
+	// Config
+	cfg, err := config.NewConfig()
+	if err != nil {
+		slog.Error("failed to load config", "error", err)
+	}
+
+	// Database
+	ctx := context.Background()
+	db, err := database.Connect(ctx, cfg.Db)
+	if err != nil {
+		slog.Error("error connecting to database", "error", err)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			slog.Error("error closing database connection", "error", err)
+		}
+	}()
+
+	// App
+	app := app.NewApp(cfg, db.Conn())
+	app.Build()
+
+	// var createUserUseCase inbound.CreateUserUseCase
+	// if err := app.Identity.Invoke(func(uc inbound.CreateUserUseCase) {
+	// 	createUserUseCase = uc
+	// }); err != nil {
+	// 	slog.Error("error getting CreateUserUseCase", "error", err)
+	// }
+
+	// user, err := createUserUseCase.Execute(ctx, inbound.CreateUserUseCaseInput{
+	// 	IdentityDocument: core.IdentityDocument("465464654646"),
+	// 	Name:             "Marcelo",
+	// 	Email:            core.Email("marcelo@email.com"),
+	// 	Password:         "password",
+	// })
+	// if err != nil {
+	// 	slog.Error("error creating user", "error", err)
+	// 	return err
+	// }
+
+	// slog.Info("user created", "user", user.User.ID.Int())
 
 	return nil
 }
